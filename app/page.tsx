e6f324bc30b2f68d13ca3d099e1e1822c7e051c2
peteraphillips <p.phillips59311@gmail.com>
2025-11-07 15:37:34 +0000

    understanding state handling

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,26 @@
-import Image from "next/image";
+'use client';
 
-export function MyButton() {
-  return <button>Click Me</button>;
+import { useState } from 'react';
+
+function MyButton({count, onClick}) {
+
+  return <button onClick={onClick}>Clicked {count} times</button>;
 }
 
 export default function Home() {
+
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+    console.log(`Button clicked ${count + 1} times`);
+  }
+
   return (
     <div>
       <h1>Hello World</h1>
-      <MyButton />
+      <MyButton count={count} onClick={handleClick} />
+      <MyButton count={count} onClick={handleClick} />
     </div>
   );
 }
